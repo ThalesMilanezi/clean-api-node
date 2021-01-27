d589073b3c74468d27b39ddc447a1375b96f6e7c
Thales Milanezi <milanezitf@gmail.com>
2021-01-27 14:21:55 -0300

    test: ensure DbAddAccount returns an account on sucess

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -109,4 +109,20 @@ describe("DbAddAccount UseCase", () => {
     await expect(promise).rejects.toThrow();
   });
 
+  test("Should returns an account on sucess", async () => {
+    const { sut } = makeSut();
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    };
+    const account = await sut.add(accountData);
+    expect(account).toEqual({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_email",
+      password: "hashed_password",
+    });
+  });
+
 });
