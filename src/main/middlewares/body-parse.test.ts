1694d8aa4da154d17ca62fd59d433c10439d9b07
Thales Milanezi <milanezitf@gmail.com>
2021-02-03 23:16:21 -0300

    fix: tests middlewares

--- src/main/middlewares/body-parse.test.ts
+++ src/main/middlewares/body-parse.test.ts
@@ -2,10 +2,10 @@ import request from "supertest";
 import app from "../config/app";
 
 describe("Body Parser Middleware", () => {
-  test("", async () => {
+  test("Should parse body as json", async () => {
     app.post("/test_body_parser", (req, res) => {
-        res.send()
-    });
+        res.send(req.body)
+    }); 
     await request(app)
     .post("/test_body_parser")
     .send({name: 'Rodrigo'})
--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -1,15 +1,17 @@
-import request from "supertest";
-import app from "../config/app";
+import app from '../../main/config/app'
 
-describe("CORS Middleware", () => {
-  test("should enable cors", async () => {
-    app.get("/test_cors", (req, res) => {
-        res.send()
-    });
+import request from 'supertest'
+
+describe('CORS Middleware', () => {
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
     await request(app)
-    .get("/test_cors")
-    .expect('access-control-allow-origin','*')
-    .expect('access-control-allow-methods','*')
-    .expect('access-control-allow-headers','*')
-  });
-});
+      .get('/test_cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
+
